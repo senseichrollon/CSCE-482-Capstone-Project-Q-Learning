cb5916cd10b36749ffa78c6be907f7d333d9a92d
Stephanie Lam <stephanie.lam_3211@tamu.edu>
2024-04-04 14:17:20 -0500

    added google API w/ Steve

--- frontend.py
+++ frontend.py
@@ -1,6 +1,21 @@
 import tkinter as tk
 import subprocess
 from tkinter import ttk
+from googletrans import Translator
+from googletrans import LANGUAGES
+
+language_dict = {name: code for code, name in LANGUAGES.items()}
+def translate_labels(root, dest_language):
+    translator = Translator()
+    print(dest_language)
+    dest_language_code = language_dict[dest_language]
+
+    for widget in root.winfo_children():
+        if isinstance(widget, (tk.Label, tk.Button)):
+            original_text = widget.cget("text")
+            translated_text = translator.translate(original_text, dest=dest_language_code).text
+            widget.config(text=translated_text)
+
 
 def run_backend():
     arg1 = entry1.get()
@@ -12,7 +27,7 @@ def run_backend():
 
 
 root = tk.Tk()
-root.title("Frontend GUI")
+root.title("CARLA User Interface")
 
 style = ttk.Style(root)
 style.theme_use('classic')
@@ -45,7 +60,24 @@ entry5.grid(row=4, column=1, padx=5, pady=5)
 
 # Create 'Run' button
 run_button = tk.Button(root, text="Run Backend", command=run_backend)
-run_button.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+run_button.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+
+# Create language dropdown menu
+languages = list(LANGUAGES.values())
+language_select_label = tk.Label(root, text="Select Language:")
+language_select_label.grid(row=5, column=0, padx=5, pady=5)
+#languages = [ 'fr', 'en', 'sp']  # Add more languages as needed
+language_select = ttk.Combobox(root, values=languages)
+
+language_select.grid(row=5, column=1, padx=5, pady=5)
+
+
+language_select.current(0)  # Set default language
+language = language_select.current(0)  # Set default language
+#root.language = language_select.get()
+# Create 'Translate' button
+translate_button = tk.Button(root, text="Translate", command=lambda:translate_labels(root, language_select.get()))
+translate_button.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
 
 
 # Start the GUI event loop
